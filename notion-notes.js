ebcb8a15d8e7cac201f226c47cd1d7fadad5de9a
judosia <judosia666999@gmail.com>
2026-07-25 11:38:51 +0800

    Update notion-notes.js

--- notion-notes.js
+++ notion-notes.js
@@ -16,7 +16,7 @@
           <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/>
           <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/>
         </svg>
-        <span>我的筆記</span>
+        <span>Notes</span>
         <span class="notion-note-time">${formatTime(note.updated)}</span>
       </div>
       <div class="notion-note-body">${note.contentHtml || ''}</div>
